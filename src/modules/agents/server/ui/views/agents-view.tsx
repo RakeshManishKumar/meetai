5fd02f9490087e26a931adce5953a2984624f40f
RakeshManishKumar <mks893767@gmail.com>
2025-08-17 18:55:47 +0530

    08: Resonsive Dialog

--- src/modules/agents/server/ui/views/agents-view.tsx
+++ src/modules/agents/server/ui/views/agents-view.tsx
@@ -4,12 +4,24 @@ import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { LoadingState } from "@/components/loadingState"
 import { ErrorState } from "@/components/errorState"
+import { ResponsiveDialog } from "@/components/responsive-dialog"
+import { Button } from "@/components/ui/button"
 export const AgentsView = () => {
     const trpc = useTRPC()
     const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions())
     
     return (
         <div>
+            <ResponsiveDialog
+            title="Responsive test"
+            description="Responsive test"
+            open={true}
+            onOpenChange={() => {}}
+            >
+               <Button>
+                Some actions
+               </Button>
+            </ResponsiveDialog>
             {JSON.stringify(data,null,2)}
         </div>
     )   ;
